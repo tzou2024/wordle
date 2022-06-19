45ecab32a2219dc452a0d4de67bd920d99da8346
Trevor Zou <tzou@olin.edu>
2022-06-18 22:35:34 -0500

    add keyboard

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ const board = document.getElementById('board')
 const titleContainer = document.getElementById('title-container')
 const startGameButton = document.getElementById('start-game-button')
 const container = document.getElementById('container')
+const keyboardContianer = document.getElementById('keyboard-container')
+const keyboard = document.getElementById('keyboard')
 
 function generateBoxes() {
     console.log('starting')
@@ -15,5 +17,71 @@ function generateBoxes() {
     startGameButton.style.display = "none"
     container.style.height = "100%"
 }
+const keys = [
+    ['Q',
+    'W',
+    'E',
+    'R',
+    'T',
+    'Y',
+    'U',
+    'I',
+    'O',
+    'P'],
+    ['A',
+    'S',
+    'D',
+    'F',
+    'G',
+    'H',
+    'J',
+    'K',
+    'L'],
+    ['ENTER',
+    'Z',
+    'X',
+    'C',
+    'V',
+    'B',
+    'N',
+    'M',
+    'back']
+]
 
-startGameButton.addEventListener('click', generateBoxes)
+function generateKeyboard(){
+    keys.forEach(row => {
+        const keyRow = document.createElement('div')
+        keyRow.classList.add('keyRow')
+        row.forEach(key =>{
+        
+        const keyButton = document.createElement('button')
+        keyButton.innerText = key
+        keyButton.setAttribute('id', key)
+        keyButton.classList.add('key')
+        keyButton.addEventListener('click', () =>{
+            console.log(key)
+        })
+        keyRow.appendChild(keyButton)
+        })
+        keyboard.appendChild(keyRow)
+    })
+    const back = document.getElementById('back')
+    back.innerText = ""
+    
+    back.innerHTML = `<i class="material-icons">&#xe14a;</i>`
+}
+
+function startSequence() {
+    generateBoxes()
+    generateKeyboard()
+}
+
+
+startGameButton.addEventListener('click',
+    startSequence
+ )
+
+
+
+
+
