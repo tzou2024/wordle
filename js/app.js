9ac77e4040d50e5137acb38e9b2c923c42a12437
Trevor Zou <tzou@olin.edu>
2022-06-21 13:34:37 -0500

    add row continuation and word checking

--- js/app.js
+++ js/app.js
@@ -152,16 +152,26 @@ function compareWords(guess){
         if(parsedguess[i] == word[i]){
             let checkingKey = document.getElementById(`${row * lettercount + i}`)
             checkingKey.style.backgroundColor = "green"
+            checkingKey.style.color = "white"
+            checkingKey.style.borderStyle = "hidden"
+
         }
         else if(word.split("").includes(parsedguess[i])){
             let checkingKey = document.getElementById(`${row * lettercount + i}`)
             checkingKey.style.backgroundColor = "yellow"
+            checkingKey.style.color = "white"
+            checkingKey.style.borderStyle = "hidden"
         }
         else{
             let checkingKey = document.getElementById(`${row * lettercount + i}`)
             checkingKey.style.backgroundColor = "red"
+            checkingKey.style.color = "white"
+            checkingKey.style.borderStyle = "hidden"
+
         }
     }
+    row++
+    element = 0
     
 }
 
